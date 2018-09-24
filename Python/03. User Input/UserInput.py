3c835f915aa000387599f1cc5e1001db056cc346
Dimas <dimaswicaksono93@gmail.com>
2018-09-24 23:01:12 +0800

    add user input

---
+++ Python/03. User Input/UserInput.py
@@ -0,0 +1,22 @@
+##
+#   UserInput.py
+#   Demonstrate user input interaction
+#
+#   @package	LearnCode
+#   @author		Dimas Wicaksono
+#   @since		2018-45-24
+##
+
+# get user input
+user_name   = input("Enter your name: ")
+user_age    = input("Enter your age: ")
+print("Hello, "+ user_name)
+print("You're "+ user_age + " years old.")
+
+# simple calculation
+num1    = input("Enter a number: ")
+num2    = input("Enter another number: ")
+
+# convert string to integer
+result  = int(num1) + int(num2)
+print(result)
