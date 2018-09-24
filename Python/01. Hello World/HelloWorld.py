3817dad343fada2b7b76d1d0ce50f07963de55b2
Dimas <dimaswicaksono93@gmail.com>
2018-09-24 21:21:30 +0800

    initial commit

---
+++ Python/01. Hello World/HelloWorld.py
@@ -0,0 +1,10 @@
+##
+#   HelloWorld.py
+#   Simple hello world script
+#
+#   @package	LearnCOde
+#   @author		Dimas Wicaksono
+#   @since		2018-16-24
+##
+
+print("This is LearnCode. Hello World!")
