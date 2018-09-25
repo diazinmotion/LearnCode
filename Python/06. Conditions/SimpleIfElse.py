dca5f34ab2eb0a3b5b14404127c833343e68c606
Dimas <dimaswicaksono93@gmail.com>
2018-09-25 12:14:12 +0800

    update simple if statement

--- Python/06. Conditions/SimpleIfElse.py
+++ Python/06. Conditions/SimpleIfElse.py
@@ -7,3 +7,32 @@
 #   @since		2018-57-25
 ##
 
+"""
+    Condition:
+    i wake up
+    if i hungry
+        i eat breakfast
+
+    if it's weekday
+        i'll go to work
+        if it's cloudy
+            i'll bring an umbrella
+        otherwise
+            i'll bring sunglasses
+"""
+
+# predefine condition
+person_name = "John"
+is_male = True
+is_tall = False
+
+# user identity
+print("Hello " + person_name)
+if is_male and is_tall:
+    print("you're a male and tall")
+elif is_male and not(is_tall):
+    print("you're a male and short")
+elif not(is_male) and is_tall:
+    print("you're not a male and tall")
+else:
+    print("you're not a male and not tall")
