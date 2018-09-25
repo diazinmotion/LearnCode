2492eca2a95d9e66847ad917317318016541d478
Dimas <dimaswicaksono93@gmail.com>
2018-09-25 11:27:59 +0800

    update list example

--- Python/04. Lists/ListFunction.py
+++ Python/04. Lists/ListFunction.py
@@ -39,3 +39,20 @@ lucky_words = ["run", "alpha", "bravo"]
 lucky_words.pop()
 print(lucky_words)
 
+# get index of element
+print(lucky_words.index("alpha"))
+
+# count number of element appearance
+print(lucky_words.count("alpha"))
+
+# sort a list
+lucky_words.sort()
+print(lucky_words)
+
+# reverse the order of a list
+lucky_words.reverse()
+print(lucky_words)
+
+# copy a list
+lucky_words2 = lucky_words.copy()
+print(lucky_words2)
