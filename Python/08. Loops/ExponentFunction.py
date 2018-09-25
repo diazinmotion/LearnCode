b8d3cdbf87227cd5a8cbb35feccc844c0bad2ced
Dimas <dimaswicaksono93@gmail.com>
2018-09-25 18:46:23 +0800

    move exponent function to loop directory

---
+++ Python/08. Loops/ExponentFunction.py
@@ -0,0 +1,11 @@
+##
+#   SimpleExponent.py
+#   Simple exponent function based on loop
+#
+#   @package	LearnCode
+#   @author		Dimas Wicaksono
+#   @since		2018-43-25
+##
+
+# make a simple exponent method of 2 parameter
+
