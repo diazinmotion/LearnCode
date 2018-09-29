7505618729ac081f9c9da0d0b11aa1a9b0493873
Dimas <dimaswicaksono93@gmail.com>
2018-09-29 11:09:31 +0800

    add setter/getter properties

--- Python/12. Class & Object/Questions.py
+++ Python/12. Class & Object/Questions.py
@@ -13,3 +13,77 @@ class Questions:
     def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
+
+
+class Queue:
+    # this is super private attribute
+    __spam = "Eggs"
+
+    def __init__(self, content):
+        self._hiddenlist = list(content)
+
+    def add_hidden_list(self, num):
+        self._hiddenlist.insert(0,1)
+
+    def pop_hidden_list(self):
+        self._hiddenlist.pop()
+
+    def print_spam(self):
+        print(self.__spam);
+
+    # output the hiddenlist
+    def __repr__(self):
+        return "Queue: ({})".format(self._hiddenlist)
+
+
+# demo for class method
+class Rectangle:
+
+    def __init__(self, x, y):
+        self.width = x
+        self.height = y
+
+    def calculate_area(self):
+        return self.height * self.width
+
+    # class method
+    # a method that act like a separated class
+    @classmethod
+    def new_square(cls, side_length):
+        # cls() is like we call new class initialization
+        # in this case it's class(x, y)
+        return cls(side_length, side_length)
+
+
+# demo for static method
+class Pizza:
+    def __init__(self, toppings):
+        self.toppings = toppings
+        self._allowed_pineapple = False
+
+    # static method
+    # a method that act like normal method
+    @staticmethod
+    def validate_toppings(topping):
+        if topping == 'pineapple':
+            raise ValueError("No Pineapples!")
+        else:
+            return True
+
+    # property getter & setter
+    # property just like constant, we can't set it by default
+    # we need to use setter and getter
+    @property
+    def pineapple_allowed(self):
+        return self._allowed_pineapple
+
+
+    # property setter
+    @pineapple_allowed.setter
+    def pineapple_allowed(self, value):
+        if value:
+            password = input("Input Password: ")
+            if password == "password":
+                self._allowed_pineapple = value
+            else:
+                raise ValueError("Not Allowed")
--- Python/12. Class & Object/app.py
+++ Python/12. Class & Object/app.py
@@ -7,7 +7,7 @@
 #   @since		2018-05-25
 ##
 
-from Questions import Questions
+from Questions import Questions, Queue, Rectangle, Pizza
 
 # series of questions
 questions_prompt = [
@@ -32,5 +32,41 @@ def run_test(question):
 
     print("You got " + str(score) + "/" + str(len(question)) + " correct.")
 
-run_test(questions)
+# run_test(questions)
 
+quest = Queue([0, 2, 3, 4])
+print(quest)
+quest.add_hidden_list(1)
+print(quest)
+quest.pop_hidden_list()
+print(quest)
+# directly access hiddenlist (private)
+print(quest._hiddenlist)
+quest.print_spam()
+# this use to access super private data
+print(quest._Queue__spam)
+
+# class method
+rectangle = Rectangle(4, 5)
+print(rectangle.calculate_area())
+
+# call class method
+square = Rectangle.new_square(5)
+print(square.calculate_area())
+
+# static method
+# call static method
+# we can call static method without initialize the class instance first
+# they usually not affect the main class attributes
+toppings = ['spam', 'cheese', 'onion']
+if all(Pizza.validate_toppings(i) for i in toppings):
+    pizza = Pizza(toppings)
+
+# demo of setter / getter property
+pizza = Pizza(["cheese", "tomato"])
+# access property
+print(pizza.pineapple_allowed)
+
+# change the property value
+pizza.pineapple_allowed = True
+print(pizza.pineapple_allowed)
