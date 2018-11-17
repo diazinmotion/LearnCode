334af371b29e3fcaabfecdaad28e57b42918973b
Dimas <dimaswicaksono93@gmail.com>
2018-11-17 22:43:56 +0800

    add generic class

--- TypeScript/10. Generics/app.js
+++ TypeScript/10. Generics/app.js
@@ -1,2 +1,47 @@
 "use strict";
-console.log("Hello World!");
+// simple Generics
+function echo(data) {
+    return data;
+}
+console.log(echo("Max"));
+console.log(echo(27));
+console.log(echo({ name: "Max", age: 27 }));
+// the better way for generic function
+// <T> just like in C# and Java
+// for function, instead of any type we better use generic type
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho("Max").length);
+console.log(betterEcho(27));
+console.log(betterEcho({ name: "Max", age: 27 }));
+// built-in generics
+var testResults2 = [1.94, 2.33];
+testResults2.push(-2.99);
+// testResults.push("Anna"); // will error because of type mismatch
+// arrays
+function printAll(args) {
+    args.forEach(function (element) { return console.log(element); });
+}
+printAll(['Apple', 'Orange', 'Banana']);
+// generic types (ES6)
+var echo2 = betterEcho;
+console.log(echo2("Something"));
+// generic class
+var SimpleMath = /** @class */ (function () {
+    function SimpleMath() {
+    }
+    SimpleMath.prototype.calculate = function () {
+        // return this.baseValue * this.multiplyValue;
+        // we should change it to this:
+        // because typescript detect that whenever our class type is string
+        // this mehtod wont be funct.
+        // we add plus (+) to set this as number
+        return +this.baseValue * +this.multiplyValue;
+    };
+    return SimpleMath;
+}());
+var simpleMath = new SimpleMath();
+simpleMath.baseValue = 10;
+simpleMath.multiplyValue = 10;
+console.log(simpleMath.calculate());
--- TypeScript/10. Generics/app.ts
+++ TypeScript/10. Generics/app.ts
@@ -1,1 +1,51 @@
-console.log("Hello World!")
+// simple Generics
+function echo(data: any) {
+    return data;
+}
+console.log(echo("Max"));
+console.log(echo(27));
+console.log(echo({name: "Max", age: 27}));
+
+// the better way for generic function
+// <T> just like in C# and Java
+// for function, instead of any type we better use generic type
+function betterEcho<T>(data: T) {
+    return data;
+}
+console.log(betterEcho("Max").length);
+console.log(betterEcho<number>(27));
+console.log(betterEcho({name: "Max", age: 27}));
+
+// built-in generics
+const testResults2: Array<number> = [1.94, 2.33];
+testResults2.push(-2.99);
+// testResults.push("Anna"); // will error because of type mismatch
+
+// arrays
+function printAll<T>(args: T[]) {
+    args.forEach((element) => console.log(element));
+}
+printAll<string>(['Apple', 'Orange', 'Banana']);
+
+// generic types (ES6)
+const echo2: <T>(data: T) => T = betterEcho;
+console.log(echo2<string>("Something"));
+
+// generic class
+class SimpleMath<T> {
+    baseValue: T;
+    multiplyValue: T;
+    calculate(): number {
+        // return this.baseValue * this.multiplyValue;
+        // we should change it to this:
+        // because typescript detect that whenever our class type is string
+        // this mehtod wont be funct.
+        // we add plus (+) to set this as number
+        return +this.baseValue * +this.multiplyValue;
+    }
+}
+
+const simpleMath = new SimpleMath();
+simpleMath.baseValue = 10;
+simpleMath.multiplyValue = 10;
+console.log(simpleMath.calculate());
