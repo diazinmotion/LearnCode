80d0e1cb6a82be953d09ccb86ceed3ce1e6d2dd2
Dimas <dimaswicaksono93@gmail.com>
2018-11-17 10:22:59 +0800

    add functions and compiler chapters

---
+++ TypeScript/04. Compiler/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var myName6 = "Max";
+var myAge6 = 27;
+var anything;
+anything = 12;
+// this even if it's error the compiler
+// will still compile it to the js file
+// if noEmitonError set to true
+// myName6 = 22;
---
+++ TypeScript/04. Compiler/app.ts
@@ -0,0 +1,10 @@
+let myName6 = "Max";
+let myAge6 = 27;
+let anything;
+
+anything = 12;
+
+// this even if it's error the compiler
+// will still compile it to the js file
+// if noEmitonError set to true
+// myName6 = 22;
